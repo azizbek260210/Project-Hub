3a4b24b42a1d7b8ca716324a5db0ca8dfb4b9809
azizbek260210 <azizbek@260210.com>
2024-05-12 22:06:37 +0500

    Project hub site NT month-8 task-9

--- api/urls.py
+++ api/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('our-team/', views.TeamList.as_view()),
+    path('our-team/', views.TeamsList.as_view()),
     path('portfolio/', views.PortfolioList.as_view()),
     path('lists-of-vacancy/', views.VacancyList.as_view()),
     path('vacancy-detail/<int:pk>/', views.VacancyDetail.as_view()),
